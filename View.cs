42d2618c4ad4109d499f03b3560ac2e75babd546
Manav Misra <manavm@visionwebsoft.com>
2022-03-15 14:02:55 -0500

    Complete View methods

--- View.cs
+++ View.cs
@@ -3,8 +3,19 @@ namespace GuessingGameDemo
 {
     public class View
     {
-        public View()
+        public static void DisplayMessage(string message)
         {
+            Console.WriteLine(message);
+        }
+
+        public static int GetGuess(int maxNum)
+        {
+            return Validation.PromptUser4Int("Enter your guess: ", 1, maxNum);
+        }
+
+        public static int GetMaxNum()
+        {
+            return Validation.PromptUser4Int("Enter the max number to guess.");
         }
     }
 }
